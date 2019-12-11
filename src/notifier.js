20421d35ca367a59282f2710fc578920a0f6a238
Andy Gray <andy@whatsthescore.co.uk>
2019-12-11 10:54:57 +0000

    notifier key

--- src/notifier.js
+++ src/notifier.js
@@ -2,7 +2,7 @@ import notify from 'bnc-notify';
 
 export default (network = 1) => {
   return notify({
-    dappId: '1cd447b9-145b-4ed5-a67b-c04e12b304e6', // FIXME
+    dappId: '3049aef8-aac4-4afa-8558-7d0ce37e9c07', // generated via BlockRocket's account
     networkId: network,
     darkMode: true,
     desktopPosition: "bottomRight",
